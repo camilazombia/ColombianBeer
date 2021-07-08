f61b08fd63d904ab63128d755d3d18d98a8ce4c9
camilazombia <camilagomez2307@gmail.com>
2021-07-07 21:36:39 -0500

    Update 7 junio

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 function validate(){
-    var ageValidate = parseInt(document.getElementById("form-age").value);
+    var ageValidate = parseInt(document.querySelector('#form-age').value);
+    var name = document.querySelector('.form-name')
 
     if(ageValidate  <18){
-        let wrong = document.querySelector('#form-wrong');
-        wrong.innerHTML = `<h4 class='form-wrong__text'>*Eres menor.No puedes Ingresar*</h4>`;
+        let wrong = document.querySelector('#form-wrong')
+        wrong.innerHTML = `<h4 class=form-wrong__text>*Eres menor.No puedes Ingresar*</h4>`;
     }
     else if(ageValidate > 18) {
         window.location="continue.html";
     }
     else{
-        let wrong = document.getElementById("form-wrong");
+        let wrong = document.querySelector('#form-wrong');
         wrong.innerHTML = `<h4 class='form-wrong__text'>*Ingresa un carácter válido*</h4>`;
     }
 }
@@ -17,7 +18,29 @@ function validate(){
 document.querySelector(".form-btn").addEventListener("click", validate);
 
 function ValidateBtn() {
-  document.querySelector(".form-btn");
+    document.querySelector(".form-btn");
 }
 
+//animaciones
 
+document.querySelector(".svg-beer__01").addEventListener("mouseover", mouseOver);
+document.querySelector(".svg-beer__01").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    $(".svg-beer__01").css({ 
+    "fill": "red", 
+    "stroke": "#231F20", 
+    "stroke-miterlimit": "10",
+});
+}
+
+function mouseOut() {
+    $(".svg-beer__01").css({ 
+    "fill": "#FA88C3", 
+    "stroke": "#231F20", 
+    "stroke-miterlimit": "10",
+});
+}
+
+$(".sello").animate({   width:'300px'   },
+                        "slow",);
--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-class producto{
-    constructor(nombre, tipo, grados, precio,imagen) {
+window.onload = () => {
+    document.querySelector('[data-target="#exampleModal"]').click();
+  }
+
+  
+    class producto{
+    constructor(id, nombre, tipo, grados, precio,imagen) {
+        this.id = id;
         this.nombre = nombre;
         this.tipo = tipo;
         this.grados = parseFloat(grados);
@@ -18,51 +24,62 @@ class producto{
 }
 
 const productos = [];
-productos.push(new producto("Cerveza Andina", "lager", "4.2", "12900", "https://via.placeholder.com/150"));
-productos.push(new producto("Miller Lite", "lager", "4.2", "12900","https://via.placeholder.com/150"));
-productos.push(new producto("Miller Genuine Draft", "lager", "4.7", "24000", "https://via.placeholder.com/150" ));
-productos.push(new producto("Heineken", "lager", "5", "11800","https://via.placeholder.com/150"));
-productos.push(new producto("Coors Ligth", "lager", "4.2", "13800", "https://via.placeholder.com/150"));
-productos.push(new producto("Sol", "lager", "4.5", "21000", "https://via.placeholder.com/150"));
-productos.push(new producto("Tecate", "lager", "4.5", "13200","https://via.placeholder.com/150" ));
-
+productos.push(new producto("1", "Bogotá", "lager", "4.2", "12900", "https://via.placeholder.com/150"));
+productos.push(new producto("2","Medellín", "lager", "4.2", "12900","https://via.placeholder.com/150"));
+productos.push(new producto("3","Cali", "lager", "4.7", "24000", "https://via.placeholder.com/150" ));
+productos.push(new producto("4","Cartagena", "lager", "5", "11800","https://via.placeholder.com/150"));
+productos.push(new producto("5","Bucaramanga", "lager", "4.2", "13800", "https://via.placeholder.com/150"));
+productos.push(new producto("6","Manizales", "lager", "4.5", "21000", "https://via.placeholder.com/150"));
+productos.push(new producto("7","Cúcuta", "lager", "4.5", "13200","https://via.placeholder.com/150" ));
+productos.push(new producto("8","Pereira", "lager", "4.5", "13200","https://via.placeholder.com/150" ));
+productos.push(new producto("9","Ibagué", "lager", "4.5", "13200","https://via.placeholder.com/150" ));
 
 for (const producto of productos) {
     let div = document.createElement("div");
 
-    div.className='card col-3'
+    div.className='card col-sm-12 col-md-3 cardproduct my-3'
 
     div.innerHTML = 
-    `<img class="card-img-top"src=${producto.image}>
+    `<img class="card-img-top"src="${producto.image}">
     <h3 class="card-title"> ${producto.nombre}</h3>
      <p class="card-type">  Tipo: ${producto.tipo}</p>
      <p class="card-gre">  Grados: ${producto.grados}</p>
-     <b class="card-price"> $ ${producto.precio}</b>
-     <button type="button" class="card-btn">Comprar</button>`;
+     <p class="card-price"> $ ${producto.precio}</p>
+     <button type="button" class="card-btn${producto.id} card-btn">Comprar</button>`;
      document.body.appendChild(div);
 
-    let button = document.createElement("input");
-    button.innerHTML = "Comprar";
+     $(`.card-btn${producto.id}`).on('click', function () {
+        console.log(`Compraste ${producto.nombre}`);
+        localStorage.setItem('${producto.nombre}','${producto.id}');
+
+    });
 
     const product = document.querySelector("#product")
-
     product.insertAdjacentElement("afterbegin", div);
 }
 
 
-//entrega clase 4 (Producto1/ calcular total)
-//const suma  = (a,b) => a + b;
-//const resta = (a,b) => a - b;
+//animaciones
+$(".svg-beer__01").mouseOver("mouseover");
+$(".svg-beer__01").mouseOut("mouseout");
 
-//let PrecioBase  = producto1.precio; 
-//let PrecioIva  = producto1.sumaIva; 
-//let PrecioDescuento = producto1.Descuento;
+function mouseOver() {
+$(".svg-beer__01").style.color = "red";
+}
 
-//console.log(PrecioBase)
-//console.log(PrecioIva)
-//console.log(PrecioDescuento)
+function mouseOut() {
+$(".svg-beer__01").style.color = "black";
+}
 
-//let ValorTotal = resta(suma( PrecioBase, (PrecioIva)), PrecioDescuento); 
-//console.log(ValorTotal)
+document.querySelectorAll(".svg-beer__01").addEventListener("mouseover", mouseOver);
+document.querySelectorAll(".svg-beer__01").addEventListener("mouseout", mouseOut);
 
-  
+function mouseOver() {
+  document.querySelectorAll(".svg-beer__01").style.color = "red";
+}
+
+function mouseOut() {
+  document.querySelectorAll(".svg-beer__01").style.color = "black";
+}
+
+
